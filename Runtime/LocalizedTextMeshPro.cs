6f14aef1dbf56617974df0fe8f528e189e34b9f0
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-01-31 20:41:51 +0300

    [feat] LocalizedTextMeshPro.cs

---
+++ Runtime/LocalizedTextMeshPro.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CustomLocalization.Runtime
+{
+    /// <summary>
+    ///     Localize text component.
+    /// </summary>
+    [RequireComponent(typeof(TMP_Text))]
+    internal sealed class LocalizedTextMeshPro : MonoBehaviour
+    {
+        [field: SerializeField] internal string LocalizationKey { get; private set; }
+
+        private void Start()
+        {
+            Localize();
+            LocalizationManager.OnLocalizationChanged += Localize;
+        }
+
+        private void OnDestroy()
+        {
+            LocalizationManager.OnLocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+            GetComponent<TMP_Text>().text = LocalizationManager.Localize(LocalizationKey);
+        }
+    }
+}
